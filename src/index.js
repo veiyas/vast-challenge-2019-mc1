ff20b9f89ec3e5e6afccad59705a56aa76f508b7
Algot Sandahl <algot.sandahl@gmail.com>
2021-04-27 19:37:48 +0200

    Just a loader icon for some reason

--- src/index.js
+++ src/index.js
@@ -1,19 +1,41 @@
-import { csv } from 'd3';
+import { csv, select } from 'd3';
 import './style.scss';
-import {createHeatmap} from './heatmap.js'
+import { createHeatmap } from './heatmap.js';
 // import { Tooltip, Toast, Popover } from 'bootstrap';
 const locationNames = [
-  "Palace Hills", "Northwest", "Old Town", "Safe Town", "Southwest", "Downtown", "Wilson Forest",
-  "Scenic Vista", "Broadview", "Chapparal", "Terrapin Springs", "Pepper Mill", "Cheddarford",
-  "Easton", "Weston", "Southton", "Oak Willow", "East Parton", "West Parton"]
+  'Palace Hills',
+  'Northwest',
+  'Old Town',
+  'Safe Town',
+  'Southwest',
+  'Downtown',
+  'Wilson Forest',
+  'Scenic Vista',
+  'Broadview',
+  'Chapparal',
+  'Terrapin Springs',
+  'Pepper Mill',
+  'Cheddarford',
+  'Easton',
+  'Weston',
+  'Southton',
+  'Oak Willow',
+  'East Parton',
+  'West Parton',
+];
 
 const main = async () => {
   try {
     const data = await csv('data/mc1-reports-data.csv');
 
-    for (let index = 0; index <  locationNames.length /* TO DEBUG FASTER locationNames.length*/; index++) {
-      createHeatmap(data.filter(d => d['location'] == index + 1), locationNames[index])
+    for (let index = 0; index < locationNames.length; index++) {
+      createHeatmap(
+        data.filter((d) => d['location'] == index + 1),
+        locationNames[index]
+      );
     }
+
+    select('#loading-icon').remove();
   } catch (err) {
     console.error(err);
   }
