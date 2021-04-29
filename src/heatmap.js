936b69b956b69f2411e3e61397da849741ed034a
veiyas <david.rk97@gmail.com>
2021-04-29 11:41:28 +0200

    Added option to switch between full and average plotting, added an axis to the bottom of the plotted data.

--- src/heatmap.js
+++ src/heatmap.js
@@ -2,13 +2,15 @@ import { select, scaleBand, scaleLinear, axisBottom, axisLeft, timeFormat, axisT
 
 const colorArray = ["#1f77b4","#ff7f0e","#2ca02c","#d62728","#9467bd","#8c564b","#e377c2","#7f7f7f","#bcbd22","#17becf"];
 
-export function createHeatmap(data, location, slot) {
+export function createHeatmap(data, location, slot, isAvgPlot) {
+    console.log("Creating heatmap")
+
     const locationNameWidth = 115;
     var margin = {top: 0, right: 50, bottom: 0, left: 115},
     width = 1200 - margin.left - margin.right,
-    height = 75 - margin.top - margin.bottom;
+    height =  isAvgPlot ? 40 - margin.top - margin.bottom : 75 - margin.top - margin.bottom;
     
-    if (slot === 0) { select("#heatmaps").append("svg").attr("id", "xAxisSVG"); }
+    if (slot === 0) { select("#heatmaps").html("").append("svg").attr("id", "xAxisSVG"); }
     // Create div to contain heatmap with a paragraph before it
     var locationDiv = select("#heatmaps");
     locationDiv
@@ -45,10 +47,6 @@ export function createHeatmap(data, location, slot) {
     const end = new Date('2020-04-10T00:00:00')
     for (var timePoints = [], dt = new Date('2020-04-06T00:00:00'); dt <= end; dt.setMinutes(dt.getMinutes() + 5)) { timePoints.push(new Date(dt)); }
     
-    // Labels of row and columns
-    const myVars = ["Sewer & Water", "Power", "Roads & Bridges", "Medical", "Buildings", "Shake Intensity"]
-    const csvVariableNames = ["sewer_and_water", "power", "roads_and_bridges", "medical", "buildings", "shake_intensity"]
-
     // Build X scales and axis:
     var x = scaleBand()
     .range([ 0, width ])
@@ -63,9 +61,12 @@ export function createHeatmap(data, location, slot) {
         .append("g")
         .attr("transform", "translate(" + horizontalOffset + "," + (xAxisBarHeight-0.5) + ")")
         .call(axisTop(x).
-                tickValues(x.domain().filter(function(d) { return d.getMinutes() == 0 && d.getSeconds() == 0 && (d.getHours() % 8 == 0)}))
-                .tickFormat(x => timeFormat("%B %d, %H:%M")(x)))
+        tickValues(x.domain().filter(function(d) { return d.getMinutes() == 0 && d.getSeconds() == 0 && (d.getHours() % 8 == 0)}))
+        .tickFormat(x => timeFormat("%B %d, %H:%M")(x)))
     }
+
+    const myVars = isAvgPlot ? ["Severity"] : ["Sewer & Water", "Power", "Roads & Bridges", "Medical", "Buildings", "Shake Intensity"];
+    const csvVariableNames = ["sewer_and_water", "power", "roads_and_bridges", "medical", "buildings", "shake_intensity"]
 
     // Build Y scales and axis:
     var y = scaleBand()
@@ -76,20 +77,45 @@ export function createHeatmap(data, location, slot) {
     .call(axisLeft(y));
 
     // Build color scale
-    console.log(colorArray[slot % colorArray.length])
     var myColor = scaleLinear()
     .range(["white", colorArray[slot % colorArray.length]])
     .domain([1,10])
 
     data.forEach(obj => {
-        const dataTimePoint = new Date(obj.time)        
-        for (let index = 0; index < myVars.length; index++) {
+        const dataTimePoint = new Date(obj.time);
+        if (isAvgPlot) {
+            var avgSeverity = 0;
+            for (let index = 0; index < csvVariableNames.length; index++) {
+                avgSeverity += obj[csvVariableNames[index]] / csvVariableNames.length;
+            }
             context.beginPath();
-            context.rect(x(dataTimePoint), y(myVars[index]), x.bandwidth(), y.bandwidth());
-            context.fillStyle = myColor(obj[csvVariableNames[index]])
+            context.rect(x(dataTimePoint), y(myVars[0]), x.bandwidth(), y.bandwidth());
+            context.fillStyle = myColor(avgSeverity);
             context.fill()
+        } else {
+            for (let index = 0; index < myVars.length; index++) {
+                context.beginPath();
+                context.rect(x(dataTimePoint), y(myVars[index]), x.bandwidth(), y.bandwidth());
+                context.fillStyle = myColor(obj[csvVariableNames[index]])
+                context.fill()
+            }
         }
     })
+
+    if (slot == 18) { // Print x-axis on top
+        const horizontalOffset = margin.left + locationNameWidth;
+        const xAxisBarHeight = 20;
+        select("#heatmaps")
+        .append("svg")
+        .attr("id", "botttom-bar")
+        .attr("width", width + margin.left + margin.right + locationNameWidth)
+        .attr("height", 20)
+        .append("g")
+        .attr("transform", "translate(" + horizontalOffset + ", 0)")
+        .call(axisBottom(x).
+        tickValues(x.domain().filter(function(d) { return d.getMinutes() == 0 && d.getSeconds() == 0 && (d.getHours() % 8 == 0)}))
+        .tickFormat(x => timeFormat("%B %d, %H:%M")(x)))
+    }
 
     return "Heatmap created"
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import './style.scss';
 import { createHeatmap } from './heatmap.js';
 // import { Tooltip, Toast, Popover } from 'bootstrap';
 
+const main = async () => {
+  try {
+    const data = await csv('data/mc1-reports-data.csv');
+    document.getElementById("full-plot").onclick = function() {constructHeatmaps(data);}
+    document.getElementById("avg-plot").onclick = function() {constructHeatmaps(data);}
+    
+    constructHeatmaps(data);
+
+    select('#loading-icon').remove();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
 const locationNames = [
   'Palace Hills',
   'Northwest',
@@ -25,22 +39,18 @@ const locationNames = [
   'West Parton',
 ];
 
-const main = async () => {
-  try {
-    const data = await csv('data/mc1-reports-data.csv');
+function constructHeatmaps(data) {
+  document.getElementById("loading-text").textContent = "Loading...";
 
-    for (let index = 0; index < locationNames.length; index++) {
-      createHeatmap(
-        data.filter((d) => d['location'] == index + 1),
-        locationNames[index],
-        index
-      );
-    }
-
-    select('#loading-icon').remove();
-  } catch (err) {
-    console.error(err);
+  for (let index = 0; index < locationNames.length; index++) {
+    createHeatmap(
+      data.filter((d) => d['location'] == index + 1),
+      locationNames[index],
+      index,
+      document.getElementById("avg-plot").checked
+    );
   }
-};
+  document.getElementById("loading-text").textContent = "";
+}
 
 main();
